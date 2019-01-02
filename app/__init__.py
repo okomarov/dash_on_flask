fdf336735f864174f893e2356a678aa95f352c97
okomarov <oleg.komarov11@gmail.com>
2019-01-02 13:12:13 +0000

    separate server from dashapp

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
-import dash
+
 from flask import Flask
-from flask_login import login_required
 
 from config import BaseConfig
 
@@ -8,14 +7,11 @@ from config import BaseConfig
 def create_app():
     server = Flask(__name__)
     server.config.from_object(BaseConfig)
-    dashapp = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')
 
     register_extensions(server)
     register_blueprints(server)
 
-    protect_dashviews(dashapp)
-
-    return server, dashapp
+    return server
 
 
 def register_extensions(server):
@@ -33,9 +29,3 @@ def register_blueprints(server):
     from app.webapp import server_bp
 
     server.register_blueprint(server_bp)
-
-
-def protect_dashviews(dashapp):
-    for view_func in dashapp.server.view_functions:
-        if view_func.startswith(dashapp.url_base_pathname):
-            dashapp.server.view_functions[view_func] = login_required(dashapp.server.view_functions[view_func])
--- dashapp.py
+++ dashapp.py
@@ -1,15 +1,29 @@
 from datetime import datetime as dt
 
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas_datareader as pdr
 from dash.dependencies import Input
 from dash.dependencies import Output
+from flask_login import login_required
 
 from app import create_app
 
 
-server, dashapp = create_app()
+def protect_dashviews(dashapp):
+    for view_func in dashapp.server.view_functions:
+        if view_func.startswith(dashapp.url_base_pathname):
+            dashapp.server.view_functions[view_func] = login_required(dashapp.server.view_functions[view_func])
+
+
+server = create_app()
+
+# =============================
+# Dash app
+# =============================
+dashapp = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')
+protect_dashviews(dashapp)
 
 dashapp.layout = html.Div([
     html.H1('Stock Tickers'),
@@ -36,3 +50,8 @@ def update_graph(selected_dropdown_value):
         }],
         'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
     }
+
+# =============================
+# Another dash app
+# =============================
+# ...
