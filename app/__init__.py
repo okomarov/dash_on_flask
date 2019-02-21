a8b635f35653b001b96c02f3776e2101b1687228
okomarov <oleg.komarov11@gmail.com>
2019-02-21 18:17:10 +0000

    Organize dash app into its own subfolder

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
+import dash
 from flask import Flask
+from flask.helpers import get_root_path
+from flask_login import login_required
 
 from config import BaseConfig
 
@@ -7,10 +10,36 @@ def create_app():
     server = Flask(__name__)
     server.config.from_object(BaseConfig)
 
+    register_dashapps(server)
     register_extensions(server)
     register_blueprints(server)
 
     return server
+
+
+def register_dashapps(app):
+    from app.dashapp1.layout import layout
+    from app.dashapp1.callbacks import register_callbacks
+
+    # Meta tags for viewport responsiveness
+    meta_viewport = {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
+
+    dashapp1 = dash.Dash(__name__,
+                         server=app,
+                         url_base_pathname='/dashboard/',
+                         assets_folder=get_root_path(__name__) + '/dashboard/assets/',
+                         meta_tags=[meta_viewport])
+
+    dashapp1.title = 'Dashapp 1'
+    dashapp1.layout = layout
+    register_callbacks(dashapp1)
+    _protect_dashviews(dashapp1)
+
+
+def _protect_dashviews(dashapp):
+    for view_func in dashapp.server.view_functions:
+        if view_func.startswith(dashapp.url_base_pathname):
+            dashapp.server.view_functions[view_func] = login_required(dashapp.server.view_functions[view_func])
 
 
 def register_extensions(server):
